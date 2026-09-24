1c23c284295bd6a7ec95c90dcb4216e9f479159c
erivaniacasanova-jpg <erivaniacasanova@gmail.com>
2026-09-24 18:50:37 -0300

    Update representantes.ts

--- representantes.ts
+++ representantes.ts
@@ -6,37 +6,37 @@ export interface Representante {
 }
 
 export const REPRESENTANTES: Record<string, Representante> = {
-  "110956": {
+  "francisco": {
     id: "110956",
-    nome: "Francisco Eliedisom Dos Santos", // Coloque seu nome aqui
-    whatsapp: "5584981321396", // Coloque o SEU WhatsApp com DDD (somente números)
-    linkCadastro: "https://associarse.com.br"
+    nome: "Francisco Eliedisom Dos Santos",
+    whatsapp: "5584981321396",
+    linkCadastro: "https://associarse.com.br/110956" // ou https://associarse.com.br
   },
-  "134684": {
+  "william": {
     id: "134684",
     nome: "William Dos Santos Pessoa",
     whatsapp: "5521969400194",
     linkCadastro: "https://associarse.com.br/134684"
   },
-  "135302": {
+  "erivania": {
     id: "135302",
     nome: "Antonia Erivania Delmiro Jacinto",
     whatsapp: "558498410187",
     linkCadastro: "https://associarse.com.br/135302"
   },
-  "153542": {
+  "aline": {
     id: "153542",
     nome: "Aline Aparecida Melo",
     whatsapp: "553193371195",
     linkCadastro: "https://associarse.com.br/153542"
   },
-  "191651": {
+  "gilmar": {
     id: "191651",
     nome: "Gilmar Goncalves De Paula",
     whatsapp: "5522998914547",
     linkCadastro: "https://associarse.com.br/191651"
   },
-  "88389": {
+  "wagner": {
     id: "88389",
     nome: "Wagner Cruz Vieira",
     whatsapp: "5521996098857",
@@ -44,5 +44,5 @@ export const REPRESENTANTES: Record<string, Representante> = {
   }
 }
 
-// Define o seu cadastro como o padrão da página principal (sem rota)
-export const REPRESENTANTE_PADRAO: Representante = REPRESENTANTES["110956"];
+// Define o representante padrão que aparece quando acessam a raiz (suanetturbinada.com.br)
+export const REPRESENTANTE_PADRAO: Representante = REPRESENTANTES["francisco"];
